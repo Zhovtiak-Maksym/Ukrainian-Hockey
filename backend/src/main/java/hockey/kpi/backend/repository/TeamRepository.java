d0e24e82f31f07c306957a411d74e4ea15056d2c
Zhovtiak Maksym <maxon.zhovtyak@gmail.com>
2026-05-16 23:56:50 +0300

    [Backend] Created repositories for database access (Spring Data).

---
+++ backend/src/main/java/hockey/kpi/backend/repository/TeamRepository.java
@@ -0,0 +1,13 @@
+package hockey.kpi.backend.repository;
+
+import hockey.kpi.backend.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByName(String name);
+}
